620c46808eba29776c816e635fbb01d3bcb1996a
Igor Baryshnikov <masterj63@gmail.com>
2014-03-09 21:20:33 +0400

    menu in SplashActivity disabled

--- SecondApp/src/mdev/master_j/secondapp/SplashActivity.java
+++ SecondApp/src/mdev/master_j/secondapp/SplashActivity.java
@@ -4,8 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class SplashActivity extends ActionBarActivity {
 	static final int SPLASH_RUNNING_TIME = 2000;
@@ -22,24 +20,5 @@ public class SplashActivity extends ActionBarActivity {
 				startActivity(intent);
 			}
 		}, SPLASH_RUNNING_TIME);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
 	}
 }
