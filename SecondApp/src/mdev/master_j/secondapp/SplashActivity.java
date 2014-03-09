a1f82e4ff1f970c9aada069ddae669ae9a629782
Igor Baryshnikov <masterj63@gmail.com>
2014-03-09 12:47:45 +0400

    replaced `main` with `splash`

--- SecondApp/src/mdev/master_j/secondapp/SplashActivity.java
+++ SecondApp/src/mdev/master_j/secondapp/SplashActivity.java
@@ -5,12 +5,12 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends ActionBarActivity {
+public class SplashActivity extends ActionBarActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_splash);
 	}
 
 	@Override
